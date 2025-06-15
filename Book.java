fe2042a9606964f207851621f46e4a61820b41eb
Irza Hasan <irzahasan07@gmail.com>
2025-06-15 21:27:56 +0500

    added new functionalities

--- Book.java
+++ Book.java
@@ -3,24 +3,21 @@ package ConsulnetLibrary;
 import java.util.ArrayList;
 
 public class Book {
-    ArrayList<Book> book= new ArrayList<>();
 
     int id;
     String title;
     String author;
     int quantity;
 
-    public Book(int id, String title, String author, int quantity){
+    public Book(int id, String author, String title, int quantity){
         this.id = id;
-        this.title = title;
         this.author = author;
+        this.title = title;
         this.quantity = quantity;
     }
 
     boolean isAvailable(){
-        if (quantity>=1){
-            return true;
-        }else return false;
+        return quantity >= 1;
     }
 
 
