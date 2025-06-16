fbc23eda1c5fe126145f783ac9a6dc47d96db763
Irza Hasan <irzahasan07@gmail.com>
2025-06-16 15:29:40 +0500

    checking implementation

--- Runner.java
+++ Runner.java
@@ -2,6 +2,32 @@ package ConsulnetLibrary;
 
 public class Runner {
     public static void main(String[] args) {
+        BookInventory bookInventory = new BookInventory();
+        bookInventory.addBook(1,"Nimra Ahmed", "Mushaf",5);
+        bookInventory.addBook(2,"Umaira Ahmed", "Mushaf",5);
+        bookInventory.addBook(3,"Nimra Ahmed", "Mushaf",5);
+        for (Book book: bookInventory.bookList){
+            System.out.println(book);
+        }
+
+        if(bookInventory.bookAvailability("Mushaf")){
+            bookInventory.borrowBook("Mushaf");
+        }
+
+        for (Book book: bookInventory.bookList){
+            System.out.println(book);
+        }
+
+        AccountHandler accountHandler = new AccountHandler();
+        accountHandler.addAccount(new Student("Student", 123,"Irza", "SE"));
+        accountHandler.addAccount(new Student("Student", 124,"Sara", "SE"));
+        accountHandler.addAccount(new Student("Student", 125,"Ayesha", "SE"));
+
+        for (Object e: accountHandler.userList){
+            System.out.println(e);
+        }
+        Student student=new Student("Student", 123,"Irza", "SE") ;
+        System.out.println(student.toString());
 
     }
 }
