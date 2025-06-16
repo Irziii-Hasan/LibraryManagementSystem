a439263cd838d28f995da28f1be1bffc66de81f9
Irza Hasan <irzahasan07@gmail.com>
2025-06-16 15:30:13 +0500

    create constructor

--- Student.java
+++ Student.java
@@ -3,5 +3,7 @@ package ConsulnetLibrary;
 public class Student extends User{
 
 
-
+    public Student(String userType, int userID, String name, String dept) {
+        super(userType, userID, name, dept);
+    }
 }
--- Teacher.java
+++ Teacher.java
@@ -3,4 +3,7 @@ package ConsulnetLibrary;
 public class Teacher extends User{
 
 
+    public Teacher(String userType, int userID, String name, String dept) {
+        super(userType, userID, name, dept);
+    }
 }
